b5cb0ca8c9ed8ea9c753ee5816e14d71fa51192f
Boggle-Head <b.smithers.actual@gmail.com>
2020-12-02 11:33:39 -0600

    Improved plots!

--- sensitivity.py
+++ sensitivity.py
@@ -22,30 +22,57 @@ def _load_flux(filename):
 
     return( e_reco, a_reco, flux )
 
+if True:
+    e_reco, a_reco, flux = _load_flux(filename)
 
-e_reco, a_reco, flux = _load_flux(filename)
+    energies = np.array(bhist([e_reco]).centers)
+    czeniths = np.array(bhist([a_reco]).centers)
 
-energies = np.array(bhist([e_reco]).centers)
-czeniths = np.array(bhist([a_reco]).centers)
+    from_muon, from_not = sep_by_flavor(flux)
 
-from_muon, from_not = sep_by_flavor(flux)
-
-cf = plt.contourf( energies/(1e9), czeniths, np.transpose(from_not), cmap=cm.coolwarm, locator=ticker.LogLocator())
-plt.xscale('log')
-plt.xlabel("Event Energy [GeV]", size=14)
-plt.ylabel(r"Reconstructed $\cos\theta$",size=14)
-cbar = plt.colorbar(cf,ticks=ticker.LogLocator())
-cbar.set_label(r"The Flux")
-plt.savefig("reco_flux_not_muon.png",dpi=400)
-plt.show()
+    cf = plt.pcolormesh(czeniths, energies/(1e9),  np.log10(from_not), cmap=cm.coolwarm) #, locator=ticker.LogLocator())
+    plt.yscale('log')
+    plt.ylabel("Event Energy [GeV]", size=14)
+    plt.xlabel(r"Reconstructed $\cos\theta$",size=14)
+    plt.title("Not Muons",size=14)
+    cbar = plt.colorbar() #cf,ticks=ticker.LogLocator())
+    cbar.set_label(r"$\log\Phi$ [GeV sr s cm$^{2}$]$^{-1}$")
+    plt.savefig("reco_flux_not_muon.png",dpi=400)
+    plt.show()
 
 
-cf = plt.contourf( energies/(1e9), czeniths, np.transpose(from_muon), cmap=cm.coolwarm, locator=ticker.LogLocator())
-plt.xscale('log')
-plt.xlabel("Event Energy [GeV]", size=14)
-plt.ylabel(r"Reconstructed $\cos\theta$",size=14)
-cbar = plt.colorbar(cf,ticks=ticker.LogLocator())
-cbar.set_label(r"The Flux")
-plt.savefig("reco_flux_from_muon.png",dpi=400)
-plt.show()
+    cf = plt.pcolormesh(czeniths,energies/(1e9), np.log10(from_muon), cmap=cm.coolwarm) #, locator=ticker.LogLocator())
+    plt.yscale('log')
+    plt.ylabel("Event Energy [GeV]", size=14)
+    plt.xlabel(r"Reconstructed $\cos\theta$",size=14)
+    plt.title("Muons",size=14)
+    cbar = plt.colorbar()# cf,ticks=ticker.LogLocator())
+    cbar.set_label(r"$\log\Phi$ [GeV sr s cm$^{2}$]$^{-1}$")
+    plt.savefig("reco_flux_from_muon.png",dpi=400)
+    plt.show()
+else:
+    e_reco, a_reco, flux = _load_flux(filename)
+
+    flux = flux["Mu_nuBar_NC"]
+
+    flux = np.ma.masked_where(flux<=0., flux)    
+
+    energies = np.array(bhist([e_reco]).centers)
+    czeniths = np.array(bhist([a_reco]).centers)
+
+#    flux *= energies**2
+
+#    from_muon, from_not = sep_by_flavor(flux)
+
+    cf = plt.pcolormesh( czeniths, energies/(1e9), np.log10(flux), cmap=cm.coolwarm) #, locator=ticker.LogLocator())
+    plt.yscale('log')
+    plt.ylim([10**2, 10**5])
+    plt.title("Muon-NuBar")
+    plt.ylabel("Event Energy [GeV]", size=14)
+    plt.xlabel(r"$\cos\theta_{z}$",size=14)
+    cbar = plt.colorbar() #cf,ticks=ticker.LogLocator())
+    cbar.set_label(r"$\log\Phi$ [GeV sr s cm$^{2}$]$^{-1}$")
+    plt.savefig("reco_flux_not_muon.png",dpi=400)
+    plt.show()
+
 
