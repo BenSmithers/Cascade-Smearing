2454360e7e654c54cb60e28e6c1023e42fa78ccf
Boggle-Head <b.smithers.actual@gmail.com>
2021-02-11 17:16:17 -0600

    Updated to use the newer file format

--- plotting/difference.py
+++ plotting/difference.py
@@ -1,3 +1,12 @@
+"""
+This is a script to show the difference between two saved flux datafiles 
+
+It shows a heatmap of their ratio as a function of the energy and cos(Zenith)
+Note that you need to use the centers of the bins, but there's unfortunately a little disparity between how things are saved
+
+downsized fluxes save the bin centers (TODO: fix this...)
+reconstructed fluxes save the bin edges 
+"""
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
@@ -10,6 +19,8 @@ from cascade.utils import config
 import pickle
 
 livetime = 10*3600*24*365.
+
+width = 0.1
 
 def _load_flux(name):
     f = open(name,'rb')
@@ -26,11 +37,11 @@ def _load_flux(name):
 #null_pt = gen_filename(config["datapath"], config["nu_flux"], 0.,0.,0.)
 
 null = SterileParams(0.,0.,0.,0.)
-mud = SterileParams(0., 0.13, 0.0, 1.3)
+mud = SterileParams(0., 0.1609, 0.2296, 4.47)
 eld = SterileParams(0.13, 0., 0.0, 1.3)
 
 e_reco, a_reco, flux_null = _load_flux(gen_filename(config["datapath"], config["recon_flux"]+".dat", null))
-e_reco, a_reco, flux_sterile = _load_flux(gen_filename(config["datapath"], config["recon_flux"]+".dat", eld))
+e_reco, a_reco, flux_sterile = _load_flux(gen_filename(config["datapath"], config["recon_flux"]+".dat", mud))
 
 ex = list(flux_null.keys())[0]
 
@@ -39,7 +50,7 @@ sterile_total = np.zeros(shape = np.shape(flux_null[ex]))
 
 just_nubar = False
 
-keep_key = "E"
+keep_key = "Tau"
 for key in flux_null.keys():
     if just_nubar and (keep_key not in key):
         print("Skip {}".format(key))
@@ -53,7 +64,7 @@ ratio = sterile_total / null_total
 energies = np.array(bhist([e_reco]).centers)
 czeniths = np.array(bhist([a_reco]).centers)
 
-cf = plt.pcolormesh(czeniths, energies/(1e9), ratio, cmap=cm.coolwarm, vmin=0.99, vmax=1.01)
+cf = plt.pcolormesh(czeniths, energies/(1e9), ratio, cmap=cm.coolwarm, vmin=1.0-width, vmax=1.0+width)
 plt.yscale('log')
 plt.ylabel("Reco Energy [GeV]", size=14)
 plt.xlabel(r"Reco $\cos\theta$",size=14)
@@ -61,6 +72,6 @@ if just_nubar:
     plt.title("Only Looking at: {}".format(keep_key),size=14)
 cbar = plt.colorbar() #cf,ticks=ticker.LogLocator())
 cbar.set_label(r"Sterile Flux / Null Flux")
-plt.savefig("flux_ratio"+ ("_nubar" if just_nubar else "") +("_{}".format(keep_key) if just_nubar else "")+".png",dpi=400)
+plt.savefig("flux_ratio" +("_{}".format(keep_key) if just_nubar else "")+"_muonD.png",dpi=400)
 plt.show()
 
