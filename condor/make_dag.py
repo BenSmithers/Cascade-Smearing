19260e77ccc0f0c666d893011bf2474b73b808da
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2021-03-18 09:23:11 -0500

    updated to make all the needed fluxes

--- condor/make_dag.py
+++ condor/make_dag.py
@@ -1,25 +1,30 @@
 import numpy as np
+from math import pi
 
-n_grid = 3
-theta13s = np.linspace(0, 2, n_grid)
-theta23s = np.linspace(0, 2, n_grid)
-msq3s = np.linspace(0,2.4,n_grid)
+n_grid = 20
+theta03s = np.linspace(0, pi, n_grid) #el
+theta13c = 0.160875
+span = 0.05
+theta13s = [0.0,0.05, 0.1, 0.160875, 0.2]
+#theta13s = np.linspace(theta13c-span , theta13c+span, 10)
+theta23s = np.linspace(0, 40*pi/180., n_grid) #tau
+msq = 4.47
 
 filename = "genflux.dag"
 obj = open(filename, "wt")
 
 counter = 0
-for theta13 in theta13s:
+for theta03 in theta03s:
     for theat23 in theta23s:
-        for msq3 in msq3s:
+        for theat13 in theta13s:
             obj.write("JOB  genflux{:06d}  genflux.submit\n".format(counter))
             counter+=1
 
 counter = 0
-for theta13 in theta13s:
+for theta03 in theta03s:
     for theta23 in theta23s:
-        for msq3 in msq3s:
-            obj.write('VARS  genflux{:06d}  arg=" {} {} {}"\n'.format(counter, theta13, theta23, msq3))
+        for theta13 in theta13s:
+            obj.write('VARS  genflux{:06d}  arg=" {} {} {} {}"\n'.format(counter,theta03, theta13, theta23, msq))
             counter+=1
 
 obj.close()
