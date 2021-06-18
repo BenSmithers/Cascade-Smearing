6c8f3bdc27a85d71f2882eaeab790ac1a25f2c2a
Boggle-Head <b.smithers.actual@gmail.com>
2021-06-18 11:26:33 -0500

    fixed errors, streamlined code

--- sensitivity/make_from_mc.py
+++ sensitivity/make_from_mc.py
@@ -28,19 +28,17 @@ def build_mc_flux(*dataobjs):
     """
     file_dir = "/home/benito/Downloads/IC86SterileNeutrinoDataRelease/monte_carlo/NuFSGenMC_nominal.dat"
     
-    bars = ["nu", "nuBar"]
-
     def metaflux(energy, angle, key):
         net_f = 0.0
         for dobj in dataobjs:
-            net_f += dobj.get_flux((1e9)*energy, key, angle=np.cos(angle))
+            net_f = net_f + dobj.get_flux((1e9)*energy, key, angle=angle)
         return net_f
 
     filename = "effective_area.nu_mu.txt"
     area_data = quickload(os.path.join(os.path.join(config["datapath"], "charm_search_supplemental/"), filename))
 
     e_edges = area_data["e_reco"]
-    a_edges = np.arccos(area_data["cos_th"])
+    a_edges = area_data["cos_th"]
     net_flux = np.zeros(shape=(28,10))
 
 
@@ -55,8 +53,15 @@ def build_mc_flux(*dataobjs):
         if parsed is None:
             continue # comment line
 
-        flux_here = metaflux(parsed[3], parsed[4], "Mu_nu_CC")
-        flux_here+= metaflux(parsed[3], parsed[4], "Mu_nuBar_CC")
+        if parsed[0]<0:
+            key = "Mu_nuBar_CC"
+        else:
+            key = "Mu_nu_CC"
+
+        flux_here = metaflux(parsed[3], parsed[4], key)
+
+        if flux_here==0.0:
+            continue
 
         if parsed[1]<e_edges[0] or parsed[1]>e_edges[-1]:
             continue
@@ -66,11 +71,11 @@ def build_mc_flux(*dataobjs):
         i_a = get_loc(parsed[2], a_edges)[0]
 
         # let's let this event add tothe total in this bin!         
-        net_flux[i_e][i_a] = net_flux[i_e][i_a] + parsed[5]*flux_here
+        net_flux[i_e][i_a] = net_flux[i_e][i_a] + 8*parsed[5]*flux_here
         
     return {
             "e_edges": e_edges,
             "a_edges": a_edges,
-            "event_rate": 10*net_flux,
-            "stat_err": np.sqrt(10*net_flux)
+            "event_rate": net_flux,
+            "stat_err": np.sqrt(net_flux)
             }
--- sensitivity/tests/mc_check_ratio.py
+++ sensitivity/tests/mc_check_ratio.py
@@ -1,29 +1,49 @@
 from cascade.utils import Data, SterileParams, gen_filename, config
 from cascade.sensitivity.make_from_mc import build_mc_flux
-from cascade.sensitivity.eff_area_reader import quickload
-
 
 import numpy as np
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 import os
+import pickle
 
-# load the fluxes 
-null_f = gen_filename(config["datapath"], "raw_det_flux.dat", SterileParams())
-ster_f = gen_filename(config["datapath"], "raw_det_flux.dat", SterileParams(0.0, 0.1609, 0.0, 4.7))
-null = Data(null_f)
-ster = Data(ster_f)
+# pass the fluxes to the MC reader
+# it bins each of the events weight*flux 
+# so we get an array of N_events in each bin
+#   alternatively just load the thingy if this is already done 
+def load(null_bool):
+    filename = "null_exp.dat" if null_bool else "ster_exp.dat"
 
-# get the flux in the centers of each of the bins
-null_flux_d = build_mc_flux(null)
-ster_flux_d = build_mc_flux(ster)
+    if False: #os.path.exists(filename):
+        f = open(filename,'rb')
+        data = pickle.load(f)
+        f.close()
+        return data
+    else:
+        if null_bool:
+            params = SterileParams()
+        else:
+            params = SterileParams(0.0, 0.1609, 0.0, 4.7)
+        # load the fluxes 
+        fu_filename = gen_filename(config["datapath"], "raw_det_flux.dat", params)
+        flux = Data(fu_filename)
+
+        data = build_mc_flux(flux)
+        f = open(filename, 'wb')
+        pickle.dump(data, f, -1)
+        f.close()
+        return data
+
+null_flux_d = load(True)
+ster_flux_d = load(False)
+
 
 null_flux = null_flux_d["event_rate"]
-ster_flux = null_flux_d["event_rate"]
+ster_flux = ster_flux_d["event_rate"]
 
 e_edges = null_flux_d["e_edges"]
-a_edges = np.cos(null_flux_d["a_edges"])
+a_edges = null_flux_d["a_edges"]
 
 plt.pcolormesh(a_edges, e_edges, ster_flux/null_flux)
 plt.colorbar()
