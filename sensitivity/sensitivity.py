af7ae489f0e34fb1cc0f2c2fe73f2c4df6d39567
Boggle-Head <b.smithers.actual@gmail.com>
2021-07-12 14:37:59 -0500

    forcefully flatten binned histograms

--- sensitivity/sensitivity.py
+++ sensitivity/sensitivity.py
@@ -18,6 +18,8 @@ from cascade.sensitivity.systematic_unc import astro_norm_unc
 from cascade.sensitivity.systematic_unc import astro_shift_unc, cr_perturb
 from cascade.sensitivity.systematic_unc import ice_grad_0, ice_grad_1
 
+from cascade.sensitivity.generate_all_integrated_fluxes import make_meta_flux
+
 from math import sqrt, pi, exp, log
 from math import lgamma
 twopi = sqrt(2/pi)
@@ -52,47 +54,82 @@ def showplot(exp, thing, title):
     plt.show()
 
 class _generic_LLHMachine:
-    def __init__(self, expectation=SterileParams(), use_syst=True):
+    def __init__(self, expectation=SterileParams(), use_syst=True, flatten=False):
         if not isinstance(expectation, SterileParams):
             raise TypeError("Expected {}, got {}".format(SterileParams, type(expectation)))
         if not isinstance(use_syst, bool):
             raise TypeError("Expected {}, got {}".format(bool, type(use_syst)))
+        if not isinstance(flatten, bool):
+            raise TypeError("Expected {}, got {}".format(bool, type(flatten)))
         self._use_systematics=use_syst
+        self._flatten = flatten
+        if not os.path.exists(self.f_name):
+            make_meta_flux(expectation, self.use_mc)
         self._configure()
- 
-    def _configure(self):
+
+    def set_expectation(self, f_expect, scale_to_n_years=None):
+        self.f_name = f_expect
+
         f = open(self.f_name, 'rb')
         self.expectation = pickle.load(f)
         f.close()
-
-
-        #showplot(self.expectation, self.astr_norm_shift, "Astro Norm")
-        self.astr_gamma_shift = astro_shift_unc(use_mc=self.use_mc)
-        #showplot(self.expectation, self.astr_gamma_shift, "Astro Gamma")
-
-        self.cr_norm_shift = cr_perturb(dnorm=0.05, use_mc = self.use_mc)
-        self.cr_gamma_shift = cr_perturb(dgamma=0.012, use_mc = self.use_mc)
-        #showplot(self.expectation, self.cr_norm_shift, "CR Norm")
-        #showplot(self.expectation, self.cr_gamma_shift, "CR Gamma")
-
-        self.ice_grad_0 = ice_grad_0(self.expectation)
-        self.ice_grad_1 = ice_grad_1(self.expectation)
-        #showplot(self.expectation, self.ice_grad_0, "Icegrad 0")
-        #showplot(self.expectation, self.ice_grad_1, "Icegrad 1")
-
+        
         # distinctly add up the plus/minus errors in quadrature 
-        self._net_error_m = self.expectation["stat_err"]    
-        self._net_error_p = self.expectation["stat_err"]
+        if scale_to_n_years is not None:
+            if not isinstance(scale_to_n_years, (int, float)):
+                raise TypeError("Expected {} or {}, got {}".format(int, float, type(scale_to_n_years)))
 
-        if self.use_systematics:
-            self._net_error_m = self._net_error_m**2 + self.astr_gamma_shift[0]**2
-            self._net_error_m = self._net_error_m  + self.cr_gamma_shift[0]**2 + self.ice_grad_0[0]**2 + self.ice_grad_1[0]**2
-            self._net_error_m = np.sqrt(self._net_error_m)
+            err = self.expectation["stat_err"]*self.expectation["stat_err"]
+            if self.use_mc:
+                err = err*scale_to_n_years/8.
+                self.expectation*=scale_to_n_years/8.
+            else:
+                err = err*scale_to_n_years/10. 
+                self.expectation*=scale_to_n_years/10.
+            err = sqrt(err)
 
-            self._net_error_p = self._net_error_p**2 + self.astr_gamma_shift[1]**2
-            self._net_error_p = self._net_error_p  + self.cr_gamma_shift[1]**2 + self.ice_grad_0[1]**2 + self.ice_grad_1[1]**2
-            self._net_error_p = np.sqrt(self._net_error_p)
+        else:
+            err = self.expectation["stat_err"]
+
+        self._net_error_m_stat = err
+        self._net_error_p_stat = err
+
+
+    def _configure(self, scale_to_n_years=None):
+        self.set_expectation(self.f_nane, scale_to_n_years)
+        
+        if self._use_systematics:
+            #showplot(self.expectation, self.astr_norm_shift, "Astro Norm")
+            self.astr_gamma_shift = astro_shift_unc(use_mc=self.use_mc)
+            #showplot(self.expectation, self.astr_gamma_shift, "Astro Gamma")
+
+            self.cr_norm_shift = cr_perturb(dnorm=0.05, use_mc = self.use_mc)
+            self.cr_gamma_shift = cr_perturb(dgamma=0.012, use_mc = self.use_mc)
+            #showplot(self.expectation, self.cr_norm_shift, "CR Norm")
+            #showplot(self.expectation, self.cr_gamma_shift, "CR Gamma")
+
+            self.ice_grad_0 = ice_grad_0(self.expectation)
+            self.ice_grad_1 = ice_grad_1(self.expectation)
+            #showplot(self.expectation, self.ice_grad_0, "Icegrad 0")
+            #showplot(self.expectation, self.ice_grad_1, "Icegrad 1")
+
+            self._net_error_m_sys = self.astr_gamma_shift[0]**2
+            self._net_error_m_sys = self._net_error_m  + self.cr_gamma_shift[0]**2 + self.ice_grad_0[0]**2 + self.ice_grad_1[0]**2
+            self._net_error_m_sys = np.sqrt(self._net_error_m)
+
+            self._net_error_p_sys = self.astr_gamma_shift[1]**2
+            self._net_error_p_sys = self._net_error_p  + self.cr_gamma_shift[1]**2 + self.ice_grad_0[1]**2 + self.ice_grad_1[1]**2
+            self._net_error_p_sys = np.sqrt(self._net_error_p)
  
+    @property
+    def _net_error_p(self):
+        return np.sqrt(self._net_error_p_stat**2 + self._net_error_p_sys**2)
+
+    @property
+    def _net_error_m(self):
+        return np.sqrt(self._net_error_m_stat**2 + self._net_error_m_sys**2)
+
+
     def get_chi2(self, flux):
         return -2*self.get_llh(flux)
     
@@ -118,17 +155,39 @@ class _generic_LLHMachine:
         """
         llh = 0.0
         for i_e in range(len(self.expectation["e_edges"])-1):
-            for i_cth in range(len(self.expectation["a_edges"])-1):
-                llh += self._eval_llh_bin(i_cth, i_e, norm*flux["event_rate"][i_e][i_cth])
+            if self._flatten:
+                llh += self._eval_llh_bin_flat(i_e, norm*sum(flux["event_rate"][i_e]))
+            else:
+                for i_cth in range(len(self.expectation["a_edges"])-1):
+                    llh += self._eval_llh_bin(i_cth, i_e, norm*flux["event_rate"][i_e][i_cth])
         return llh
 
+    def _eval_llh_bin_flat(self, i_e, bflux):
+        """
+        Same as before, but we're flattening along the angular bins
+        """
+        our_expected = sum(self.expectation["event_rate"][i_e])
+
+        if our_expected==0.0:
+            return 0.0
+
+        if bflux<expected:
+            sigma = np.sqrt(our_expected + sum(self._net_error_m_sys[i_e]**2))
+        else:
+            sigma = np.sqrt(our_expected + sum(self._net_error_p_sys[i_e]**2))
+
+        if sigma<0:
+            raise ValueError("Found negative sigma {}".format(sigma))
+        elif sigma==0 or np.isnan(sigma):
+            print("Bad Bin: sigma {}".format(sigma))
+
+        return -0.5*(bflux - our_expected)*(bflux - our_expected)/(sigma*sigma)
 
     def _eval_llh_bin(self, i_cth, i_e, bflux):
 
         """
         We assume an asymmetric gaussian distribution
         """
-
         expected = self.expectation["event_rate"][i_e][i_cth]
 
         #cutting this out until I find a better asymmetric likelihood function
@@ -140,7 +199,7 @@ class _generic_LLHMachine:
 #        sigma = self._mean_error[i_e][i_cth]
 
         if sigma<0.0:
-            raise ValueError("Negative signa??? {}".format(sigma))
+            raise ValueError("Negative sigma??? {}".format(sigma))
 
         if expected==0.0: #  and bflux==0:
             return 0.0
@@ -158,16 +217,24 @@ class _generic_LLHMachine:
         return thingy #log(twopi/(self._net_error_m[i_e][i_cth] + self._net_error_p[i_e][i_cth])) + thingy 
 
 class LLHMachine_from_mc(_generic_LLHMachine):
-    def __init__(self, expectation=SterileParams(), use_syst=True):
+    def __init__(self, expectation=SterileParams(), use_syst=True, flatten=False):
         self.f_name = gen_filename(config["datapath"] +"/expected_fluxes_reco/", "expected_flux_from_mc.dat", expectation)
         self.use_mc = True
-        _generic_LLHMachine.__init__(self, expectation, use_syst)
+        _generic_LLHMachine.__init__(self, expectation, use_syst, flatten)
 
 class LLHMachine(_generic_LLHMachine):
-    def __init__(self, expectation=SterileParams(), use_syst=True):
+    def __init__(self, expectation=SterileParams(), use_syst=True, flatten=False):
         self.f_name = gen_filename(config["datapath"] +"/expected_fluxes_reco/", "expected_flux.dat", expectation)
         self.use_mc = False
-        _generic_LLHMachine.__init__(self, expectation, use_syst)
+        _generic_LLHMachine.__init__(self, expectation, use_syst, flatten)
+
+class LLHMachine_Data(_generic_LLHMachine):
+    def __init__(self):
+        self.f_name = os.path.join(config["datapath"],"/expected_fluxes_reco/", "data_likelihoods.dat")
+        self.use_mc = False
+        _generic_LLHMachine.__init__(self, SterileParams(), True, flatten=True)
+
+
 
 if __name__=="__main__":
     import sys
@@ -200,6 +267,9 @@ if __name__=="__main__":
                         type=str, required=False,
                         default="",
                         help="Is the point distribution linear? False means it's logarithmic with the sines")
+    parser.add_argument("-f", "--flatten", required=False, dest="flatten",
+                        type=str, default="False",
+                        help="Flatten along the cos-theta axis")
     args = parser.parse_args()
     use_mc = str(args.is_mc).lower()=='true'
     th14 = args.th14
@@ -207,15 +277,18 @@ if __name__=="__main__":
     th34 = args.th34
     deltam = args.deltam
     use_syst = str(args.use_syst).lower()=="true"
-
-    if is_linear!="":
+    if args.is_linear!="":
         is_linear = str(args.is_linear).lower()=='true'
     else:
         is_linear = not use_mc
+    flatten = str(args.flatten).lower=="true" 
     expectation = SterileParams(theta03=th14, theta13=th24, theta23=th34, msq2=deltam)
 
     print("{}sing MC fluxes".format("U" if use_mc else "Not u"))
     print("Using expectation {}".format(expectation))
+    print("Using {} distribution of param points".format("linear" if is_linear else "logarithmic"))
+    print("{}sing systematics".format("U" if use_syst else "Not u"))
+    print("{}lattening".format("F" if flatten else "Not f"))
 
     # we will now build up an llh machine, and build up all the likelihood values 
     likelihooder = LLHMachine_from_mc(expectation, use_syst) if use_mc else LLHMachine(expectation,use_syst)
@@ -256,10 +329,11 @@ if __name__=="__main__":
 
                 f_name = gen_filename(config["datapath"] +"/expected_fluxes_reco/", f_name_init, pam)
                 index = th24 + th34*len(theta24s) + msq*len(theta24s)*len(theta34s)
+                if (skipped+found)%2000==0:
+                    print("Skipped {}, Found {}".format(skipped, found))
+
                 if not os.path.exists(f_name):
                     skipped+=1 
-                    if skipped%1000==0:
-                        print("Skipped {}, Found {}".format(skipped, found))
                     likelihoods[index][3] = 0.0
                     chi2[th24][th34][msq] = 1e8 #arbitrarily big...
                 else:
@@ -308,9 +382,9 @@ if __name__=="__main__":
             }
     suffix = "_from_mc" if use_mc else ""
     suffix += "" if use_syst else "_nosys"
-
+    suffix += "" if flatten else "_flat"
     filename = gen_filename(config["datapath"]+"/expectations/", "cummulative_probs"+suffix+".dat", expectation)
-
+    print("Writing Sensitivity File to {}".format(filename))
     f = open(filename,'wb')
     pickle.dump(likelihood_dict, f, -1)
     f.close()
