b981c85b961842bf0ba58e0c9f983016d7247ee8
Benjamin Smithers <benjamin.smithers@mavs.uta.edu>
2021-08-24 13:59:24 -0500

    better way of generating expectations from the mc

--- sensitivity/condor/make_dag.py
+++ sensitivity/condor/make_dag.py
@@ -1,25 +1,37 @@
 import numpy as np
 from math import pi
 
-n_grid = 20
+is_mc = True
+
+n_grid = 1
 theta03 =0
 
-theta13s = np.linspace(0, 0.5*pi, 1)
-theta23s = np.linspace(0, 0.5*pi, 1)
+# sin^2(2theta) over 1e-3, 1e0
 
-filename = "susflux.dag"
+#theta13s = np.linspace(0, 0.5*pi, n_grid)
+#theta23s = np.linspace(0, 0.5*pi, n_grid)
+
+theta13s = np.arcsin(np.sqrt(np.logspace(-3, 0, n_grid)))/2.0
+theta23s = np.arcsin(np.sqrt(np.logspace(-3, 0, n_grid)))/2.0
+
+# use the zero point! 
+
+theta13s = np.concatenate((np.array([0]) , theta13s))
+theta23s = np.concatenate((np.array([0]) , theta23s))
+
+filename = "susflux{}.dag".format("_from_mc" if is_mc else "")
 obj = open(filename, "wt")
 
 counter = 0
 for theat23 in theta23s:
     for theat13 in theta13s:
-        obj.write("JOB  susflux{:06d}  genflux.submit\n".format(counter))
+        obj.write("JOB  susflux{:06d}  genflux{}.submit\n".format(counter, "_from_mc" if is_mc else ""))
         counter+=1
 
 counter = 0
 for theta23 in theta23s:
     for theta13 in theta13s:
-        obj.write('VARS  susflux{:06d}  arg=" {} {} {} {}"\n'.format(counter, theta13, theta23))
+        obj.write('VARS  susflux{:06d}  arg=" {} {} {}"\n'.format(counter, theta13, theta23, 1 if is_mc else 0))
         counter+=1
 
 obj.close()
